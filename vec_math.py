4275922bac683548faaf973659da55f65706317a
poludmik <michael.poludin@gmail.com>
2023-06-27 19:53:14 +0200

    One sentence argmax(cosine_sim) classifier on Q50__.xlsx.

--- vec_math.py
+++ vec_math.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import spatial
 import nltk
 import fasttext
+import pandas as pd
 
 from nltk import word_tokenize
 nltk.download('punkt')
@@ -26,3 +27,19 @@ def tokenize_sentence(sentence: str, use_nltk=False) -> list[str]:
 def get_average_vector(m: fasttext.FastText, sentence: str) -> np.ndarray:
     tokens = tokenize_sentence(sentence, use_nltk=False)
     return np.average([m.get_word_vector(t) for t in tokens], axis=0)
+
+
+def classify_one_sentence(df, m, sentence):
+    class_of_max_similarity = 0
+    question_max = ""
+    max_similarity = -np.inf
+    for index, row in df.iterrows():
+        similarity = cosine_sim_scipy(get_average_vector(m, row['question']), get_average_vector(m, sentence))
+        if similarity > max_similarity:
+            max_similarity = similarity
+            question_max = row['question']
+            class_of_max_similarity = row['class']
+    return class_of_max_similarity, question_max
+
+
+
